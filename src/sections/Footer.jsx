f9cb11cb32eac6abae4e26280a831449946c4bc3
LogicIsPlantingBomb <logicunit64@gmail.com>
2024-11-03 11:57:33 +0530

    linksChangedagain

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
+        <a href="https://github.com/LogicIsPlantingBomb" className="social-icon">
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a className="social-icon">
           <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a className="social-icon">
           <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 Dhruv Bhardwaj. All rights reserved.</p>
